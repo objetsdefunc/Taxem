51a5932e382ee0bb8226fe4f53e8b6f873a885d7
John <63595764+objetdefunc@users.noreply.github.com>
2020-11-13 11:55:18 +0000

    Something on screen

---
+++ Taxem/AppBootStrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxem
+{
+    class AppBootStrapper : BootstrapperBase
+    {
+        public AppBootStrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<RootViewModel>();
+        }
+    }
+}
---
+++ Taxem/RootView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Taxem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class RootView : UserControl
+    {
+        public RootView()
+        {
+            InitializeComponent();
+        }
+    }
+}
