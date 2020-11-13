6fe1aa6944fdadba61a3b5dcb6a92f6514ade658
John <63595764+objetdefunc@users.noreply.github.com>
2020-11-13 12:36:58 +0000

    Reading a csv - Tidy stuff up next

--- Taxem/RootViewModel.cs
+++ Taxem/RootViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,32 @@ namespace Taxem
 {
     class RootViewModel : Caliburn.Micro.Screen
     {
+        private List<string> fills = new List<string>();
+
+        public RootViewModel()
+        {
+            var reader = new StreamReader(File.OpenRead(@"D:\Taxes\Crypto\fills.csv"));
+
+            var lines = new List<string>();
+            while(!reader.EndOfStream)
+            {
+                lines.Add(reader.ReadLine());
+            }
+
+            fills = lines;
+        }
+
+        public List<string> Fills
+        {
+            get => fills;
+            set
+            {
+                if (value != fills)
+                {
+                    fills = value;
+                    NotifyOfPropertyChange(() => Fills);
+                }
+            }
+        }
     }
 }
